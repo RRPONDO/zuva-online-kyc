d7354f6bdeba6cfb7e728c83edd12f1ab715bc56
Ronald Pondo <rrpondo@gmail.com>
2024-08-23 12:05:12 +0200

    adjust authOptions export functionality

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import NavigationForm from "../clientComponents/navigationLinks";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../api/auth/[...nextauth]/options";
 import prisma from "@/lib/prisma";
 import {
   Link,
